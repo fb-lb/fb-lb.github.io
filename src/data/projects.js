127bc3b83695ba513ebbdb5ea9204079fd4b989d
fb.lb <fb.lubre@free.fr>
2026-03-18 17:59:48 +0100

    Modify jsx project page. Add css for project page. Add data in data/projects.js for knowledge project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,36 +2,50 @@ const projects = {
     knowledge: {
         name: "Knowledge",
         image: {
-            source: "",
+            sourcePortrait: "/assets/img/project/knowledge/knowledge-portrait.webp",
+            sourceLandscape: "/assets/img/project/knowledge/knowledge-landscape.webp",
             alternative: "une image",
         },
         descriptions: [
             [
-                {type: "text", content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida lorem ante, hendrerit euismod ante feugiat in. Proin ut ante tempor, ullamcorper dolor vitae, interdum metus. Nam lobortis nisi id dui interdum laoreet sit amet sed lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean diam quam, sodales at fermentum nec, aliquet et erat. Morbi volutpat pretium consequat. Nullam accumsan orci justo, et varius libero fringilla a. Phasellus maximus, mi at porttitor tincidunt, tortor turpis feugiat odio, in tincidunt lectus sem ut lorem." },
+                { type: "text", content : "Le site Knowledge est une plateforme d'e-learning où il est possible de créer un compte, de simuler l'achat de cours (via une version sandbox du module de paiement Stripe). A partir de là, il est possible d'accéder aux cours 'achetés' et de valider les leçons au fur et à mesure." },
+                
+                
             ],
             [
-                {type: 'text', content: "Vestibulum aliquet mi hendrerit risus tempor egestas. Sed aliquet orci eu magna sagittis interdum eget aliquam tortor. Vestibulum scelerisque et massa quis accumsan. Etiam nunc est, pulvinar id lorem eget, laoreet tempus mauris. Nunc porttitor ornare ex, in dapibus neque sollicitudin luctus. Etiam consectetur efficitur consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus metus fringilla neque dignissim ultrices. Aenean semper laoreet leo nec bibendum."},
+                { type: "text", content: "Par défaut l'identifiant et le mot de passe renseignés dans le formulaire de login permettent d'accéder à un compte administrateur (car c'est une version de présentation). Cet accès administrateur permet d'accéder à plusieurs interfaces back-offices qui sont des CRUD permettant de gérer les utilisateurs, les cours et les achats (= les accès que les utilisateurs ont à ces cours)." },
+            ],
+            [
+                { type: "text", content: "Chaque partie et tout le fonctionnement du site est expliquée sur sa page d'accueil, vous pouvez accéder au site ainsi qu'au repository GitHub via les liens ci-dessous." },
             ],
         ],
         technologies: [
             "Angular",
-            "Express",
+            "Express (en TypeScript)",
             "MySQL",
+            "EmailJS",
+            "Stripe",
+            "FontAwesome",
         ],
         notes: [
             [
-                { type: "text", content : "Début remarque 1 " },
-                { type : "link", content : "lien 1 remarque 1", source: "", target: "" },
-                { type: "text", content : " " },
-                { type : "link", content : "lien 2 remarque 1", source: "", target: "" },
-                { type : "text", content : " fin remarque 1" },
+                { type : "text", content : "Front-end hébergé via GitHub Pages." },
             ],
             [
-                { type : "text", content : "Début remarque 2 " },
-                { type : "link", content : "lien 1 remarque 2", source: "", target: "" },
-                { type: "text", content : " " },
-                { type : "link", content : "lien 2 remarque 2", source: "", target: "" },
-                { type : "text", content : " fin remarque 2" },
+                { type : "text", content : "Back-end hébergé via Render." },
+            ],
+            [
+                { type : "text", content : "Base de données hébergée via Aiven." },
+            ],
+            [
+                { type : "text", content : "Le back-end étant hébergé sur Render avec le plan gratuit. Cela implique qu'il est mis en veille après 5 minutes d'inactivité. Afin de profiter pleinement de l'expérience de l'application merci bien vouloir sortir le back-end de sa veille en cliquant " },
+                { type : "link", content : "ici", source: "https://knowledge-back-jrzv.onrender.com/", target: "_blank" },
+                { type: "text", content : " et d'attendre que la page affiche le message : \"Le back-end de l'application Knowledge est prêt à recevoir des requêtes.\" avant de vous rendre sur le front-end via le lien ci-dessous." },
+            ],
+            [
+                { type: "text", content : "Le back-end de l'application est sur un repository différent de celui du front-end. Voici donc le lien vers " },
+                { type : "link", content : "le dépôt GitHub du back-end", source: "https://github.com/fb-lb/CEF_devoirs_knowledge-back", target: "_blank" },
+                { type: "text", content : "." },
             ],
         ],
         links: {
@@ -62,6 +76,49 @@ const projects = {
     },
     cv: {
         name: "CV",
+    },
+    test: {
+        name: "Test",
+        image: {
+            sourcePortrait: "/assets/img/project/knowledge/knowledge-portrait.webp",
+            sourceLandscape: "/assets/img/project/knowledge/knowledge-landscape.webp",
+            alternative: "une image",
+        },
+        descriptions: [
+            [
+                { type: "text", content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In gravida lorem ante, hendrerit euismod ante feugiat in. Proin ut ante tempor, ullamcorper dolor vitae, interdum metus. Nam lobortis nisi id dui interdum laoreet sit amet sed lorem. Class aptent taciti sociosqu ad " },
+                { type : "link", content : "ceci est un lien", source: "", target: "" },
+                { type: "text", content : " litora torquent per conubia nostra, per inceptos himenaeos. Aenean diam quam, sodales at fermentum nec, aliquet et erat. Morbi volutpat pretium consequat. Nullam accumsan orci justo, et varius libero fringilla a. Phasellus maximus, mi at porttitor tincidunt, tortor turpis feugiat odio, in tincidunt lectus sem ut lorem." },
+            ],
+            [
+                { type: 'text', content: "Vestibulum aliquet mi hendrerit risus tempor egestas. Sed aliquet orci eu magna sagittis interdum eget aliquam tortor. Vestibulum scelerisque et massa quis accumsan. Etiam nunc est, pulvinar id lorem eget, laoreet tempus mauris. Nunc porttitor ornare ex, in dapibus neque sollicitudin luctus. Etiam consectetur efficitur consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus metus fringilla neque dignissim ultrices. Aenean semper laoreet leo nec bibendum." },
+            ],
+        ],
+        technologies: [
+            "Angular",
+            "Express",
+            "MySQL",
+        ],
+        notes: [
+            [
+                { type: "text", content : "Début remarque 1 " },
+                { type : "link", content : "lien 1 remarque 1", source: "", target: "" },
+                { type: "text", content : " " },
+                { type : "link", content : "lien 2 remarque 1", source: "", target: "" },
+                { type : "text", content : " fin remarque 1" },
+            ],
+            [
+                { type : "text", content : "Début remarque 2 " },
+                { type : "link", content : "lien 1 remarque 2", source: "", target: "" },
+                { type: "text", content : " " },
+                { type : "link", content : "lien 2 remarque 2", source: "", target: "" },
+                { type : "text", content : " fin remarque 2" },
+            ],
+        ],
+        links: {
+            github: "https://github.com/fb-lb/CEF_devoirs_knowledge-front",
+            website: "https://fb-lb.github.io/CEF_devoirs_knowledge-front/",
+        }
     },
 }
 
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,3 +1,4 @@
+import '../css/project.css';
 import { Link, useParams } from "react-router-dom";
 import projects from "../data/projects";
 
@@ -6,74 +7,80 @@ export default function Project() {
     const project = projects[projectSlug];
 
     return(
-        <main>
-            <div>
-                <Link to='/portfolio'>Retour à la liste des projets</Link>
-                <p>{'>'}</p>
-                <p>{project.name}</p>
+        <main className=" m-0 m-sm-2 mt-sm-0 m-lg-5 mt-lg-0 p-4 pt-3 p-sm-5 pt-sm-3 p-lg-5 pt-lg-3">
+            <div className="breadcrumb-trail d-flex gap-2">
+                <Link to='/portfolio' className="breadcrumb-trail__link text-primary">Retour à la liste des projets</Link>
+                <span className='breadcrumb-trail__junction'>{'>'}</span>
+                <p className='breadcrumb-trail__project'>{project.name}</p>
             </div>
-            <div>
-                <img src={project.image.source} alt={project.image.alternative}/>
-                <section>
-                    <h1>Présentation du projet {project.name}</h1>
-                    <section>
-                        <h2>Description</h2>
-                        {project.descriptions.map((d, index) => {
-                            const contents = [];
-
-                            for (let i=0; i<d.length; i++) {
-                                if (d[i].type === 'text') contents.push(d[i].content);
-                                if (d[i].type === 'link') contents.push(
-                                    <a
-                                        key={i}
-                                        href={d[i].source}
-                                        target={d[i].target}
-                                        rel={ d[i].target === "_blank" ? "noopener noreferrer" : undefined }
-                                    >
-                                        {d[i].content}
-                                    </a>
-                                )
-                            }
-                            return<li key={index}>{contents}</li>
-                        })}
-                        
-                    </section>
-                    <section>
-                        <h3>Liste des technologies utilisées :</h3>
-                        <ul>
-                            {project.technologies.map((t, index) => (
-                                <li key={index}>{t}</li>
-                            ))}
-                        </ul>
-                    </section>
-                    <section>
-                        <h3>Remarques :</h3>
-                        <ul>
-                            {project.notes.map((n, index) => {
-                                const contents = [];
-
-                                for (let i=0; i<n.length; i++) {
-                                    if(n[i].type === 'text') contents.push(n[i].content);
-                                    if (n[i].type==='link') contents.push(
-                                        <a 
-                                            key={i} 
-                                            href={n[i].source} 
-                                            target={n[i].target} 
-                                            rel={n[i].target === '_blank' ? "noopener noreferrer" : undefined}
+            <div className="project">
+                <h1 className="project__title">Présentation du projet &quot;{project.name}&quot;</h1>
+                <div className='project__line'></div>
+                <div className='project__container'>
+                    <picture className="project__picture">
+                        <source srcSet={project.image.sourcePortrait} media="(min-width: 993px)"/>
+                        <img className="project__image" src={project.image.sourceLandscape} alt={project.image.alternative}/>
+                    </picture>
+                    <section className="project__info"> 
+                        <section className="info__section description">
+                            <h2 className="description__title">Description</h2>
+                                {project.descriptions.map((d, index) => {
+                                    const contents = [];
+                                for (let i=0; i<d.length; i++) {
+                                    if (d[i].type === 'text') contents.push(d[i].content);
+                                    if (d[i].type === 'link') contents.push(
+                                        <a
+                                            key={i}
+                                            href={d[i].source}
+                                            target={d[i].target}
+                                            rel={ d[i].target === "_blank" ? "noopener noreferrer" : undefined }
+                                            className="description__link"
                                         >
-                                            {n[i].content}
+                                            {d[i].content}
                                         </a>
-                                    );
+                                    )
                                 }
-                                return <li key={index}>{contents}</li>
-                            })}
-                        </ul>
+                                return<p key={index} className="description__paragraph">{contents}</p>
+                                })}
+                        </section>
+                        <section className="info__section technology">
+                            <h3 className="technology__title">Liste des technologies utilisées</h3>
+                            <ul className="technology__list">
+                                {project.technologies.map((t, index) => (
+                                    <li key={index} className="technology__item">{t}</li>
+                                ))}
+                            </ul>
+                        </section>
+                        <section className="info__section note">
+                            <h3 className="note__title">Remarques</h3>
+                            <ul className="note__list">
+                                {project.notes.map((n, index) => {
+                                    const contents = [];
+
+                                    for (let i=0; i<n.length; i++) {
+                                        if(n[i].type === 'text') contents.push(n[i].content);
+                                        if (n[i].type==='link') contents.push(
+                                            <a 
+                                                key={i} 
+                                                href={n[i].source} 
+                                                target={n[i].target} 
+                                                rel={n[i].target === '_blank' ? "noopener noreferrer" : undefined}
+                                                className="note__link"
+                                            >
+                                                {n[i].content}
+                                            </a>
+                                        );
+                                    }
+                                    return <li key={index} className="note__item">{contents}</li>
+                                })}
+                            </ul>
+                        </section>
+                        <div className="links">
+                            <a href={project.links.github} className="links__link" target="_blank" rel="noopener noreferrer">Voir le dépôt GitHub</a>
+                            <a href={project.links.website} className="links__link" target="_blank" rel="noopener noreferrer">Accéder au site web</a>
+                        </div>
                     </section>
-                    <div>
-                        <a href={project.links.github} target="_blank" rel="noopener noreferrer">Voir le dépôt GitHub</a>
-                        <a href={project.links.website} target="_blank" rel="noopener noreferrer">Accéder au site web</a>
-                    </div>
-                </section>
+                </div> 
             </div>
         </main>
     );
